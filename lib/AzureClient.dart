6a46d04155fc0c742f2895b3f97957e7972807ea
s-haya-123 <ganbaru.wakenai@gmail.com>
2019-04-30 22:54:06 +0900

    skip encode entity if key is not exists

--- lib/AzureClient.dart
+++ lib/AzureClient.dart
@@ -28,7 +28,14 @@ class AzureRepository {
         body:file.readAsBytesSync()
     );
     print(jsonDecode(response.body));
-    print(FaceEntity.fromJson(jsonDecode(response.body)[0])._faceRectangleEntity.height);
+    print(FaceEntity.fromJson({'faceId':"a"}).faceId);
+    if (jsonDecode(response.body).length > 0) {
+      return FaceEntity.fromJson(jsonDecode(response.body)[0]);
+    } else {
+      return null;
+    }
+//    return FaceEntity.fromJson(jsonDecode(response.body));
+//    print(FaceEntity.fromJson(jsonDecode(response.body)[0])._faceRectangleEntity.height);
   }
 
 }
@@ -37,10 +44,12 @@ class FaceEntity {
   String faceId;
   FaceRectangleEntity _faceRectangleEntity;
   FaceAttributesEntity _faceAttributesEntity;
-  FaceEntity.fromJson(Map<String,dynamic> json):
-        faceId = json['faceId'],
-        _faceRectangleEntity = FaceRectangleEntity.fromJson(json['faceRectangle']),
-        _faceAttributesEntity = FaceAttributesEntity.fromJson(json['faceAttributes']);
+  FaceEntity.fromJson(Map<String,dynamic> json){
+    faceId = json.containsKey('faceId') ? json['faceId']:null;
+    _faceRectangleEntity = json.containsKey('faceRectangle') ? FaceRectangleEntity.fromJson(json['faceRectangle']):null;
+    _faceAttributesEntity = json.containsKey('faceAttributes') ? FaceAttributesEntity.fromJson(json['faceAttributes']):null;
+  }
+
   Map<String,dynamic> toJson() =>
       {
         'faceId':faceId,
@@ -53,11 +62,12 @@ class FaceRectangleEntity {
   int left;
   int width;
   int height;
-  FaceRectangleEntity.fromJson(Map<String,dynamic> json):
-    top = json['top'],
-    left = json['left'],
-    width = json['width'],
-    height = json['height'];
+  FaceRectangleEntity.fromJson(Map<String,dynamic> json){
+    top = json.containsKey('top') ? json['top']:null;
+    left = json.containsKey('left') ? json['left']:null;
+    width = json.containsKey('width') ? json['width']:null;
+    height = json.containsKey('height') ? json['height']:null;
+  }
   Map<String,dynamic> toJson() => {
     'top': top,
     'left': left,
@@ -68,7 +78,9 @@ class FaceRectangleEntity {
 
 class FaceAttributesEntity {
   EmotionEntity _emotionEntity;
-  FaceAttributesEntity.fromJson(Map<String,dynamic> json):_emotionEntity = EmotionEntity.fromJson(json['emotion']);
+  FaceAttributesEntity.fromJson(Map<String,dynamic> json){
+    _emotionEntity = json.containsKey('emotion') ? EmotionEntity.fromJson(json['emotion']):null;
+  }
   Map<String,dynamic> toJson() =>{
     'emotion': _emotionEntity
   };
@@ -82,15 +94,16 @@ class EmotionEntity {
   double neutral;
   double sadness;
   double surprise;
-  EmotionEntity.fromJson(Map<String,dynamic> json):
-    anger = json['anger'],
-    contempt = json['contempt'],
-    disgust = json['disgust'],
-    fear = json['fear'],
-    happiness = json['happiness'],
-    neutral = json['neutral'],
-    sadness = json['sadness'],
-    surprise = json['surprise'];
+  EmotionEntity.fromJson(Map<String,dynamic> json){
+    anger = json.containsKey('anger') ? json['anger']:null;
+    contempt = json.containsKey('contempt') ? json['contempt']:null;
+    disgust = json.containsKey('contempt') ? json['contempt']:null;
+    fear = json.containsKey('fear') ? json['fear']:null;
+    happiness = json.containsKey('happiness') ? json['happiness']:null;
+    neutral = json.containsKey('neutral') ? json['neutral']:null;
+    sadness = json.containsKey('sadness') ? json['sadness']:null;
+    surprise = json.containsKey('surprise') ? json['surprise']:null;
+  }
   Map<String,dynamic> toJson() =>{
     'anger': anger,
     'contempt': contempt,
